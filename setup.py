2132606def5e515091faa59250288085fb79bc9c
Kushal Das <kdas@redhat.com>
2014-10-09 02:51:09 +0530

    Adds setup script for the project.

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+"""Setup script"""
+from setuptools import setup
+
+setup(name='cloudtofedora',
+      version='0.1',
+      description="Converts Fedora Cloud instance to Fedora Server instance.",
+      long_description = "Converts Fedora Cloud instance to Fedora Server instance.",
+      platforms = ["Linux"],
+      author="Kushal Das",
+      author_email="kdas@redhat.com",
+      url="https://github.com/fedora-cloud/cloudtoserver",
+      license = "http://www.gnu.org/copyleft/gpl.html",
+      scripts = ['cloudtofedora']
+      )
